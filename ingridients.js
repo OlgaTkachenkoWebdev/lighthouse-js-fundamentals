26a8a698889d98cd0b1ac70ec935cbc78379a547
woodenonesie <olga.pi4ugina@yandex.ru>
2022-12-21 19:50:26 -0800

    banana bread

---
+++ ingridients.js
@@ -0,0 +1,15 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+for (x = 0; x < ingredients.length; x++) {
+  console.log(ingredients[x]);
+}
+
+for (a = (ingredients.length - 1); a >= 0; a--) {
+  console.log(ingredients[a]);
+}
